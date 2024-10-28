f0ea2733c6096f2d16e6af7ef8b84c7bd63a7fed
p-fernandez <pablo.fernandez.otero@gmail.com>
2024-10-28 09:44:36 +0000

    * Script to convert flac to mp3

--- flac-to-mp3.sh
+++ flac-to-mp3.sh
@@ -1,9 +1,23 @@
-read -r -a array <<< "${@}"
+# Set the input folder path as an argument
+INPUT_FOLDER=$1
 
-search="${array[@]}/*.flac"
-echo "$search"
+# Check if the input folder path is provided
+if [ -z "$INPUT_FOLDER" ]; then
+  echo "Error: Please provide the folder path as an argument."
+  exit 1
+fi
 
-for a in "$search"; do
-  echo "$a";
-  ffmpeg -i "$a" -qscale:a 0 "${a[@]/%flac/mp3}"
+# Check if the input folder exists
+if [ ! -d "$INPUT_FOLDER" ]; then
+  echo "Error: The specified folder does not exist."
+  exit 1
+fi
+
+# Iterate through the files in the input folder
+for file in "$INPUT_FOLDER"/*.flac; do
+  # Check if the item is a regular file (not a directory)
+  if [ -f "$file" ]; then
+    echo "Converting... $file"
+    ffmpeg -i "$file" -ab 320k "${file/%flac/mp3}"
+  fi
 done
